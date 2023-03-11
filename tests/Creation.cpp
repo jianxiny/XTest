1c10d4da5db4679de0e84da4035bd835c9ab848f
jianxin <1816305173@qq.com>
2023-03-11 14:03:00 +0800

    register tests with first test

---
+++ tests/Creation.cpp
@@ -0,0 +1,7 @@
+//
+// Created by cqs on 2023-03-11.
+//
+#include "../Test.h"
+#include <iostream>
+
+Test { std::cout << mName << std::endl; };
---
+++ tests/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../Test.h"
+
+
+
+// now the main file is not care about the test name
+
+int main() {
+    MereTDD::runTests();
+    return 0;
+}
