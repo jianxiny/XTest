78fb162c3fbb1262ac9269a8ad6811acdb3d4914
jianxin <1816305173@qq.com>
2023-03-15 10:02:10 +0800

    add timestamp support

--- Memo/Log.h
+++ Memo/Log.h
@@ -8,11 +8,21 @@
 #include <fstream>
 #include <iostream>
 #include <string_view>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
 
 namespace Memo {
-    inline void log(std::string_view message) {
+    inline std::fstream log() {
+        auto const now = std::chrono::system_clock::now();
+        std::time_t const tmNow = std::chrono::system_clock::to_time_t(now);
+        auto const ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
         std::fstream logFile("application.log", std::ios::app);
-        logFile << message <<std::endl;
+        logFile << std::endl
+                << std::put_time(std::gmtime(&tmNow), "%Y-%m-%dT%H:%M:%S.")
+                << std::setw(3) << std::setfill('0') << std::to_string(ms.count())
+                << " ";
+        return logFile;
     }
 }// namespace Memo
 #endif //MEMO_LOG_H
--- Memo/tests/Construction.cpp
+++ Memo/tests/Construction.cpp
@@ -9,8 +9,20 @@ TEST("Simple message can be logged") {
     std::string message = "simple";
     message += Util::randomString();
 
-    Memo::log(message);
+    Memo::log() << message << " with more text";
 
     bool  result = Util::isTextInFile(message, "application.log");
+    CONFIRM_TRUE(result);
+}
+
+TEST("Complicated message can be logged")
+{
+    std::string message = "complicated ";
+    message += Util::randomString();
+    Memo::log() << message
+            << " double=" << 3.14
+            << " quoted=" << std::quoted("in quotes");
+    bool result = Util::isTextInFile(message,
+                                     "application.log");
     CONFIRM_TRUE(result);
 }
