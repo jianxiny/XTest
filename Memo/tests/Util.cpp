dbae8312144c6697725e53e5a4832724eb4c5b5a
jianxin <1816305173@qq.com>
2023-03-15 09:46:04 +0800

    simple message can be logged

--- Memo/tests/Util.cpp
+++ Memo/tests/Util.cpp
@@ -5,12 +5,30 @@
 #include "Util.h"
 
 #include <fstream>
+#include <chrono>
 #include <random>
 
 std::string Util::randomString() {
-    return "1";
+    static bool firstCall = true;
+    static std::mt19937 rng;
+    if (firstCall) {
+        firstCall = false;
+        unsigned int seed = static_cast<int>(
+                std::chrono::system_clock::now().time_since_epoch().count()
+        );
+        rng.seed(seed);
+    }
+    std::uniform_int_distribution<std::mt19937::result_type> dist(1, 10000);
+    return std::to_string(dist(rng));
 }
 
 bool Util::isTextInFile(std::string_view text, std::string_view fileName) {
+    std::ifstream logfile(fileName.data());
+    std::string line;
+    while (std::getline(logfile, line)) {
+        if (line.find(text) != std::string::npos) {
+            return true;
+        }
+    }
     return false;
 }
